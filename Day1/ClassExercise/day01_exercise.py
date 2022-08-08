e517ede7a3fe66efc72baa4e7045226a05e21c28
Peter Bachman <peter.j.bachman@gmail.com>
2022-08-08 14:44:00 -0500

    Add current work

--- Day1/ClassExercise/day01_exercise.py
+++ Day1/ClassExercise/day01_exercise.py
@@ -4,25 +4,39 @@
 
 # Write a for loop, while loop, or function (or all three!) to create a
 # list of the first 10 numbers of the fibonacci sequence
-
-
+x = []
+for i in range(0, 10):
+  if i == 0:
+    x.append(1)
+  elif i == 1:
+    x.append(1)
+  else:
+    x.append(x[i - 1] + x[i - 2])
+x
 
 """return true if there is no e in 'word', else false"""
 def has_no_e(word):
-
+  return(not ("e" in word))
 
 """return true if there is e in 'word', else false"""
 def has_e(word):
-
+  return("e" in word)
 
 """return true if word1 contains only letters from word2, else false"""
 def uses_only(word1, word2):
+  return all(letters in word1 for letters in word2)
 
+uses_only("centaur", "cent")
 
 """return true if word1 uses all the letters in word2, else false"""
 def uses_all(word1, word2):
+  return all(letters in word2 for letters in word1)
 
+uses_all("centrau", "centaur")
 
 """true/false is the word in alphabetical order?"""
 # Hint: check the methods for lists
 def is_abecedarian(word):
+  alpWord = sorted(word)
+  orgWord = [i for i in word]
+  return alpWord == orgWord
--- Day1/Lab/lab01.py
+++ Day1/Lab/lab01.py
@@ -9,32 +9,67 @@ def binarify(num):
   """convert positive integer to base 2"""
   if num<=0: return '0'
   digits = []
+  while num != 0:
+    digits.append(num % 2)
+    num = num // 2
+  
+  digits = list(reversed(digits))
+  digits = map(str, digits)
+
   return ''.join(digits)
+
+binarify(29)
 
 def int_to_base(num, base):
   """convert positive integer to a string in any base"""
   if num<=0:  return '0' 
   digits = []
+  while num != 0:
+    digits.append(num % base)
+    num = num // base
+
+  digits = list(reversed(digits))
+  digits = map(str, digits)
+
   return ''.join(digits)
+
+int_to_base(29, 4)
 
 def base_to_int(string, base):
   """take a string-formatted number and its base and return the base-10 integer"""
   if string=="0" or base <= 0 : return 0 
-  result = 0 
-  return result 
+  result = 0
+  num = [int(i) for i in string]
+  num = list(reversed(num))
 
-def flexibase_add(str1, str2, base1, base2):
+  for i in range(0, len(string)):
+    result += num[i] * base ** i
+
+  return result
+
+base_to_int("11101", 2)
+
+def flexibase_add(str1, str2, base1, base2, base3):
   """add two numbers of different bases and return the sum"""
-  result = int_to_base(tmp, base1)
+  num1 = base_to_int(str1, base1)
+  num2 = base_to_int(str2, base2)
+  tmp = num1 + num2
+  result = int_to_base(tmp, base3)
   return result 
 
-def flexibase_multiply(str1, str2, base1, base2):
+def flexibase_multiply(str1, str2, base1, base2, base3):
   """multiply two numbers of different bases and return the product"""
-  result = int_to_base(tmp, base1)
+  num1 = base_to_int(str1, base1)
+  num2 = base_to_int(str2, base2)
+  tmp = num1 * num2
+  result = int_to_base(tmp, base3)
   return result 
 
 def romanify(num):
   """given an integer, return the Roman numeral version"""
+  numerals = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+  
+
   result = ""
   return result
 
