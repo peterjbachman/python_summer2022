d346b78aba775d2e27ce54621c672df68b43bf6c
Peter Bachman <peter.j.bachman@gmail.com>
2022-08-13 10:01:25 -0500

    Finish up print function, adjust selling functions

--- HW/hw1.py
+++ HW/hw1.py
@@ -1,29 +1,57 @@
 # Homework 1
 # Author: Peter Bachman
-# Date: 08/??/2022
+# Date: 08/13/2022
 
-# TODO Finish print function
-# TODO Format print function correctly
-# TODO Finish history function
-# TODO Create Comments for functions
-# TODO Sell Price is changed every time, not set to the class
-# TODO Create proper errors?
-
-# Import a few modules
+# Import module
 import random
 
 
 # Portfolio class
 class Portfolio:
     def __init__(self):
+        # Set up variables in the class
         self.transactions = {}
         self.historyLine = 1
         self.portfolio = {"Cash": 0}
 
     # Print what is in the portfolio
-    # NOT FINISHED
     def __str__(self):
-        report = "cash: $" + str(self.portfolio["Cash"])
+        # First create the report line
+        report = "cash: $" + str(self.portfolio["Cash"]) + "\n"
+        reportStock = "stock: "
+        reportMF = "mutual fund: "
+        firstStock = True
+        firstMF = True
+        # For each key in the dicitionary
+        for i in enumerate(self.portfolio.keys()):
+            # Exclude the dictionary value for 'Cash'
+            if i[0] == 0:
+                continue
+            name = i[1]
+            # If the dictionary value is a stock, add to the stock string
+            if self.portfolio[name][1] == "Stock":
+                # If it's the first stock to show up, append it to the string
+                # and then line break
+                if firstStock:
+                    reportStock += str(self.portfolio[name][0]) + \
+                        " " + name + "\n"
+                    firstStock = False
+                # Sort of a hacked job of doing line justifications but it
+                # works for now.
+                else:
+                    reportStock += "       " + str(self.portfolio[name][0]) + \
+                        name + "\n"
+            # Do the same with the mutual funds
+            if self.portfolio[name][1] == "Mutual Fund":
+                if firstMF:
+                    reportMF += "{:.2f}".format(self.portfolio[name][0]) + \
+                        " " + name + "\n"
+                    firstMF = False
+                else:
+                    reportMF += "             " + \
+                        "{:.2f}".format(self.portfolio[name][0]) + \
+                        " " + name + "\n"
+        report += reportStock + reportMF
         return report
 
     def addCash(self, cash):
@@ -68,23 +96,31 @@ class Portfolio:
             print("Cannot buy negative amount of shares.")
         else:
             self.portfolio["Cash"] -= amt
-            self.portfolio[mtf.name] = [amt, "Mutual Fund", mtf.sellPrice]
+            self.portfolio[mtf.name] = [amt, "Mutual Fund"]
             self.transaction(mtf.name, "Mutual Fund", "Buy", amt, 1)
 
     # Sell Mutual Fund Function
     def sellMutualFund(self, mtf, amt):
+        """Sell Mutual Funds
+
+        Args:
+            mtf (str): Name of Mutual Fund already added to portfolio
+            amt (float): non-negative amount
+        """
+        sellPrice = random.uniform(0.9, 1.2)
         if self.portfolio[mtf][0] < amt:
             print("Cannot sell more shares than you have")
         else:
-            self.portfolio["Cash"] += amt * self.portfolio[mtf][2]
+            self.portfolio["Cash"] += amt * sellPrice
             self.portfolio[mtf][0] -= amt
             self.transaction(mtf, "Mutual Fund", "Sell", amt,
-                             self.portfolio[mtf][2])
+                             sellPrice)
             # Remove value from dictionary if all shares are sold
             if self.portfolio[mtf][0] == 0:
                 self.portfolio.pop(mtf)
 
     # Buy Stock Function
+    # These are the same as the mutual fund functions.
     def buyStock(self, amt, stock):
         if amt * stock.buyPrice > self.portfolio["Cash"]:
             print("Cannot buy more shares than you can afford.")
@@ -94,18 +130,18 @@ class Portfolio:
             print("Stock amount must be an integer.")
         else:
             self.portfolio["Cash"] -= amt
-            self.portfolio[stock.name] = [amt, "Stock", stock.sellPrice]
+            self.portfolio[stock.name] = [amt, "Stock", stock.buyPrice]
             self.transaction(stock.name, "Stock", "Buy", amt, stock.buyPrice)
 
     # Sell Stock Function
     def sellStock(self, stock, amt):
+        sellPrice = self.portfolio[stock][2] * random.uniform(0.5, 1.5)
         if self.portfolio[stock][0] < amt:
             print("Cannot sell more shares than you have")
         else:
-            self.portfolio["Cash"] += amt * self.portfolio[stock][2]
+            self.portfolio["Cash"] += amt * sellPrice
             self.portfolio[stock][0] -= amt
-            self.transaction(stock, "Stock", "Sell", amt,
-                             self.portfolio[stock][2])
+            self.transaction(stock, "Stock", "Sell", amt, sellPrice)
             # Remove value from dictionary if all shares are sold
             if self.portfolio[stock][0] == 0:
                 self.portfolio.pop(stock)
@@ -121,16 +157,28 @@ class Portfolio:
                                           "Total Price": price * amt}
 
     # History Function
-    # NOT FINISHED
     def history(self):
-        print(self.transactions)
+        # For each of the keys in the transaction dictionary, print out
+        # the details of the transaction
+        for i in enumerate(self.transactions.keys()):
+            string = "Name: " + self.transactions[i[1]]["Name"] + "\n"
+            string += "    Type: " + \
+                self.transactions[i[1]]["Fund Type"] + "\n"
+            string += "    Transaction: " + \
+                self.transactions[i[1]]["Transaction Type"] + "\n"
+            string += "    Amount: " + \
+                str(self.transactions[i[1]]["Amount"]) + "\n"
+            string += "    Price Per Share: " + \
+                str(self.transactions[i[1]]["Price Per Share"]) + "\n"
+            string += "    Total Price: " + \
+                str(self.transactions[i[1]]["Total Price"]) + "\n"
+            print(string)
 
 
 # Mutual Fund Class
 class MutualFund:
     def __init__(self, symbol):
         self.name = symbol
-        self.sellPrice = random.uniform(0.9, 1.2)
 
 
 # Stock Class
@@ -138,7 +186,6 @@ class Stock:
     def __init__(self, price, symbol):
         self.name = symbol
         self.buyPrice = price
-        self.sellPrice = price * random.uniform(0.5, 1.5)
 
 
 # TEST CLASSES
