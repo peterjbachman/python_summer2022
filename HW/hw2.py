77a9afcbe4ddf170007f4eea7fe4e9b0f49e0928
Peter Bachman <peter.j.bachman@gmail.com>
2022-08-16 12:49:18 -0500

    Clean script of misc. testing

--- HW/hw2.py
+++ HW/hw2.py
@@ -13,19 +13,7 @@ import urllib.request
 import csv
 import time
 
-# Steps
-# Start with the writing a .csv file
-# Load the main page
-# For each Address:
-# - Check if date is Jan 19, 2021
-# - If it is, stop the script.
-# - If not, load the new page
-# - Grab the date, title, full text, and citation
-# - Write the .csv file
-
-# https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks
-# https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks?page=1
-
+# Start writing to .csv file
 with open('bidenAddress.csv', 'w') as f:
     w = csv.DictWriter(f, fieldnames=("date", 'title', 'text', 'footnote'))
     w.writeheader()
@@ -41,7 +29,6 @@ with open('bidenAddress.csv', 'w') as f:
             page = baseLink
         else:
             page = baseLink + "&page=" + str(j)
-        print("Page " + str(j))
         page = urllib.request.urlopen(page)
         soup = BeautifulSoup(page.read())
         # For loop for each individual page
@@ -70,7 +57,6 @@ with open('bidenAddress.csv', 'w') as f:
                     "div", {"class": "field-docs-content"}).text
             except AttributeError:
                 remark["text"] = "NA"
-            # field-docs-footnote p
             try:
                 remark["footnote"] = remarkPage.find(
                     "div", {"class": "field-docs-footnote"}).text
@@ -78,25 +64,10 @@ with open('bidenAddress.csv', 'w') as f:
                 remark["footnote"] = "NA"
 
             # Write the row to the .csv
-            print("Writing %s to the .csv" % remark["title"])
-            print("Number: %i" % i)
             w.writerow(remark)
             # Sleep at the end to prevent issues with scraping
             time.sleep(uniform(2, 5))
+        # When the loop goes through the individual pages, iterate the outer
+        # page
         j += 1
         time.sleep(uniform(2, 5))
-
-
-# # Trying to figure out how to capture these sites
-# print(soup.prettify())
-
-# soup.find_all("div", {"class": "field-title"})[9].find("a")["href"]
-
-# # Link
-# titles[9].find("a")["href"]
-
-# # Title
-# titles[9].find("a").text
-
-# # Date
-# soup.find_all("h4")[9].text
