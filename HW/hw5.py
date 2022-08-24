ffc0930d3534da27ba96534fdf2e7dc8bb0e4572
Peter Bachman <peter.j.bachman@gmail.com>
2022-08-24 15:45:47 -0500

    Finish basic functions for the linked list

--- HW/hw5.py
+++ HW/hw5.py
@@ -34,11 +34,17 @@ class LinkedList:
     def addNodeAfter(self, new_value, after_node):
         node = self.head
         i = 0
-        while i < after_node:
-            node = node.next
-            i += 1
-        oldNext = node.next
-        node.next = Node(new_value, oldNext)
+        if after_node >= self.length:
+            raise AttributeError("Position is out of bounds")
+        if after_node == self.length - 1:
+            self.addNode(new_value)
+        else:
+            while i < after_node:
+                node = node.next
+                i += 1
+            oldNext = node.next
+            node.next = Node(new_value, oldNext)
+        self.length += 1
 
     # Complexity class would be o()
     # before_node indicates the position of the node to add it after,
@@ -51,34 +57,70 @@ class LinkedList:
             i += 1
         oldNext = node.next
         node.next = Node(new_value, oldNext)
+        self.length += 1
+
+    # Complexity class would be o()
+    # node_to_remove indicates the position of the node to remove,
+    #   starting with 0
+    def removeNode(self, node_to_remove):
+        node = self.head
+        i = 0
+        if node_to_remove == 0:
+            self.head = node.next
+        else:
+
+            while i < (node_to_remove - 1):
+                node = node.next
+                i += 1
+            beforeNode = node
+            try:
+                middleNode = beforeNode.next
+                afterNode = middleNode.next
+                beforeNode.next = afterNode
+            except AttributeError:
+                beforeNode.next = None
+        self.length -= 1
+
+    # Complexity class would be o()
+    def removeNodesByValue(self, value):
+        node = self.head
+        for i in range(self.length):
+            try:
+                if node.next.value == value:
+                    self.removeNode(i + 1)
+                node = node.next
+            except AttributeError:
+                break
+            i += 1
+
+    def reverse(self):
+        prevNode = None
+        currentNode = self.head
+        tailNode = self.head
+        while currentNode is not None:
+            nextNode = currentNode.next
+            currentNode.next = prevNode
+            prevNode = currentNode
+            currentNode = nextNode
+        self.head = prevNode
+        self.tail = tailNode
 
     # Complexity class would be o()
     def __str__(self):
         node = self.head
         nonTail = True
         linkedNodes = ""
+
         while nonTail:
             linkedNodes += str(node)
-            node = node.next
-            if node is None:
+            if node.next is None or node.next == self.head:
                 nonTail = False
                 break
             else:
                 linkedNodes += " -> "
+                node = node.next
         return linkedNodes
 
     # Complexity class would be o()
     def hasCycle(self):
         return self.tail.next == self.head
-
-
-test = LinkedList(1)
-test.addNode(2)
-test.addNode(3)
-print(test)
-
-test.addNodeAfter(5, 1)
-print(test)
-
-test.addNodeBefore(7, 1)
-print(test)
