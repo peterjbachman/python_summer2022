33be0a3053a4b6f4b6aa246272dbe7c8a7a21702
Peter Bachman <peter.j.bachman@gmail.com>
2022-08-26 14:23:56 -0500

    Add error checking, comments on complexity

--- HW/hw5.py
+++ HW/hw5.py
@@ -12,26 +12,44 @@ class Node:
 
 class LinkedList:
 
-    # Complexity class would be o()
+    # Complexity class would be o(1) since it's running the same thing no
+    # matter the size
     def __init__(self, value=None):
         self.head = Node(_value=value)
         self.tail = self.head
         self.length = 1
 
-    # Complexity class would be o()
+    def __checkValid(self, value, valueType):
+        # Check if node value is an int or float
+        if valueType == "nodeValue":
+            if type(value) not in [float, int]:
+                raise TypeError("Value must be an integer or float")
+
+        # Check if node value is an integer
+        if type == "position":
+            if type(value) != int:
+                raise TypeError("Value must be a integer")
+
+    # Complexity class would be o(1) since it's running the same thing no
+    # matter the size
     def length(self):
         return (self.length)
 
-    # Complexity class would be o()
+    # Complexity class would be o(1) since it's running the same thing no
+    # matter the size
     def addNode(self, new_value):
+        self.__checkValid(new_value, "nodeValue")
         self.tail.next = Node(new_value)
         self.tail = self.tail.next
         self.length += 1
 
-    # Complexity class would be o()
+    # Complexity class would be o(n) since it would increase with time based on
+    # where the node is being placed
     # after_node indicates the position of the node to add it after,
     #   starting with 0
     def addNodeAfter(self, new_value, after_node):
+        self.__checkValid(new_value, "nodeValue")
+        self.__checkValid(after_node, "position")
         node = self.head
         i = 0
         if after_node >= self.length:
@@ -46,10 +64,13 @@ class LinkedList:
             node.next = Node(new_value, oldNext)
         self.length += 1
 
-    # Complexity class would be o()
+    # Complexity class would be o(n) since it would increase with time based on
+    # where the node is being placed
     # before_node indicates the position of the node to add it after,
     #   starting with 0
     def addNodeBefore(self, new_value, before_node):
+        self.__checkValid(new_value, "nodeValue")
+        self.__checkValid(before_node, "position")
         node = self.head
         i = 0
         while i < (before_node - 1):
@@ -59,10 +80,12 @@ class LinkedList:
         node.next = Node(new_value, oldNext)
         self.length += 1
 
-    # Complexity class would be o()
+    # Complexity class would be o(n) since it would increase with time based on
+    # where the node is being removed
     # node_to_remove indicates the position of the node to remove,
     #   starting with 0
     def removeNode(self, node_to_remove):
+        self.__checkValid(node_to_remove, "position")
         node = self.head
         i = 0
         if node_to_remove == 0:
@@ -81,8 +104,12 @@ class LinkedList:
                 beforeNode.next = None
         self.length -= 1
 
-    # Complexity class would be o()
+    # Complexity class would be o(n^2) since it would increase with time based
+    # on going through the whole list to find values to remove, and
+    # then removing them with the removeNode function.
+    # where the node is being placed
     def removeNodesByValue(self, value):
+        self.__checkValid(value, "nodeValue")
         node = self.head
         for i in range(self.length):
             try:
@@ -93,6 +120,8 @@ class LinkedList:
                 break
             i += 1
 
+    # Complexity class would be o(n) since it is dependent on the size of the
+    # linked list
     def reverse(self):
         prevNode = None
         currentNode = self.head
@@ -105,7 +134,8 @@ class LinkedList:
         self.head = prevNode
         self.tail = tailNode
 
-    # Complexity class would be o()
+    # Complexity class would be o(n) since it is dependent on the size of the
+    # linked list
     def __str__(self):
         node = self.head
         nonTail = True
@@ -121,6 +151,7 @@ class LinkedList:
                 node = node.next
         return linkedNodes
 
-    # Complexity class would be o()
+    # Complexity class would be o(1) since it takes the same amount of time
+    # independent of size
     def hasCycle(self):
         return self.tail.next == self.head
